cec4a7774560d30ebad928e473b370ef229a7498
dhantha <dhanthagunarathna@yahoo.com>
2013-11-28 18:10:54 -0800

    1st_LAB

---
+++ fahrenheitConverter/fahrenheitConverter/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace fahrenheitConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter temperature(Fahrenheit): ");
+            float originalFahrenheit = float.Parse(Console.ReadLine());
+            float Celsius = ((originalFahrenheit - 32)/9)*5;
+            Console.WriteLine(originalFahrenheit + "degrees in Fahrenheit is" + Celsius + "degrees in Celsius");
+
+            Console.WriteLine("Enter temperature(Celsius): ");
+            float originalCelsius = float.Parse(Console.ReadLine());
+            float Fahrenheit = ((originalCelsius * 9) / 5) + 32;
+
+            Console.WriteLine(originalCelsius + "degrees in Celsius is" + Fahrenheit + "degrees in Fahrenheit");
+        }
+    }
+}
